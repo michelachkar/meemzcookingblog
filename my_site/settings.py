ef57446c8cea79636e4325da613fa4927fc4ce74
Michel Achkar <machkar89@gmail.com>
2025-02-09 16:51:46 +0100

    Fix MySQL root password issue in GitHub Actions

--- my_site/settings.py
+++ my_site/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', default=False)
 
 ALLOWED_HOSTS = []
 
